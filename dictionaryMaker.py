f5e0d2d261035b6f8a7fb4a65bd54c09dae9fe7d
Sushmey <71403193+Sushmey@users.noreply.github.com>
2021-01-10 15:02:57 +0530

    Add files via upload

---
+++ dictionaryMaker.py
@@ -0,0 +1,22 @@
+filePath = "/Users/sanjaynirmal/Desktop/dictionary.json"
+f = open(filePath, "a")
+f.write('{ "object":[')
+while(True):
+	word = input("word: ")
+	if(word=="0"):
+		f.write("]}")
+		f.close()
+		break
+	meaning = input("meaning: ")
+	f.write("{ \n")
+	f.write('"word":')
+	f.write('"')
+	f.write(word)
+	f.write('"')
+	f.write(",\n")
+	f.write('"meaning":')
+	f.write('"')
+	f.write(meaning)
+	f.write('"')
+	f.write("},\n")
+
